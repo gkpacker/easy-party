0df66b44a3f794fd5f34ca5b3607c213717fcd8b
Gabriel Packer <ggkpacker@gmail.com>
2018-04-02 19:05:17 -0300

    new and create actions created for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,28 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :index, :show
   def index
-    users = User.all
-    @professionals = users.select { |user| user.professional? }
+    @professionals = User.where(role: "Professional")
   end
 
   def show
   end
 
   def new
-    user = User.new
+    @user = User.new
   end
 
   def create
-    user = User.new
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      redirect_to new_user_registration_path
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :availability, :encrypted_password, :price_per_hour, :city)
-  end
-
-  def professional?
-    self.role == "Professional"
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :availability, :price_per_hour, :city, :role)
   end
 end
