2e656c9a0ec8d06a88aa25fdcd19160e707a8fbe
Charlotte Boucher <charlotte.boucher@hec.edu>
2018-04-03 13:46:44 -0300

    professional dashboard added with controller

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,4 +1,11 @@
 class ProfessionalsController < ApplicationController
   def show
+    @professional = User.find(params[:user_id])
+  end
+
+  private
+
+  def professional_params
+    params.require(:professional).permit(:user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'professionals/show'
 
   root to: 'users#index'
   devise_for :users
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :professionals, only: [:show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
