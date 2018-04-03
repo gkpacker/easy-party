67a6e9b4c983123dc645cc3428471840a2f4c09f
Charlotte Boucher <charlotte.boucher@hec.edu>
2018-04-03 14:15:04 -0300

    add organizers controller with route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+
   root to: 'users#index'
   devise_for :users
   resources :users, only: [:index, :show] do
     resources :professionals, only: [:show]
+    resources :organizers, only: [:show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
