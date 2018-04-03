20835315190340827c3327dd8de92b6f14eeeca2
Gabriel Packer <ggkpacker@gmail.com>
2018-04-03 08:22:44 -0300

    syntax error fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index, :show
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @professionals = User.where(role: "Professional")
   end
