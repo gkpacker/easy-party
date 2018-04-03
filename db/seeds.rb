f6f2c1c1d8474a7af7de284671153aee62c82c19
Charlotte Boucher <charlotte.boucher@hec.edu>
2018-04-03 16:48:43 -0300

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,71 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
 options = ["Animação de Festas", "Assessor de Eventos", "Bandas e cantores", "Bartenders", "Brindes e lembrancinhas", "Buffet completo", "Churrasqueiro", "Confeiteira", "Decoração", "Djs", "Equipamento para festas", "Fotografia", "Garçons e Copeiras", "Gravação de vídeos", "Recepcionistas e cerimonialistas", "Segurança"]
 options.each do |option|
-  puts "Categories #{option} created"
   categorie = Category.new(name: option)
   categorie.save
 end
+puts "Done ! #{options.count} categories added to the database."
+
+organizers = []
+10.times do
+  organizer = User.new
+  organizer.first_name = Faker::Name.first_name
+  organizer.last_name = Faker::Name.last_name
+  organizer.email = Faker::Internet.email
+  organizer.password = "123456"
+  organizer.phone_number = Faker::PhoneNumber.phone_number
+  organizer.role = "Organizer"
+  organizer.save
+  organizers << organizer
+end
+puts "Done ! #{organizers.count} organizers added to the User database."
+
+professionals = []
+20.times do
+  professional = User.new
+  professional.first_name = Faker::Name.first_name
+  professional.last_name = Faker::Name.last_name
+  professional.email = Faker::Internet.email
+  professional.password = "123456"
+  professional.phone_number = Faker::PhoneNumber.phone_number
+  professional.role = "Professional"
+  random_number = (1..8).to_a.sample
+  professional.availability = %w(segunda terça quarta quinta sexta sabado domingo fds).sample(random_number)
+  professional.price_per_hour = %w(20 30 40 50 60 70 80 90 100).sample.to_i
+  professional.city = %w(Pinheiros Morumbi Itaim Campinas Sorocaba Santos Jardins).sample
+  professional.category = Category.all.sample
+  professional.save
+  professionals << professional
+end
+puts "Done ! #{professionals.count} professionals added to the User database."
+
+events = []
+titles = ["despedida de solteiro de ", "aniversario de ", "casamento de", "cocktail para", "housewarming de"]
+5.times do
+  event = Event.new
+  event.organizer = User.where(role: "Organizer").sample
+  event.title = "#{titles.sample} #{Faker::Name.first_name}"
+  event.date = Faker::Date.forward(60)
+  event.location = %w(Pinheiros Morumbi Itaim Campinas Sorocaba Santos Jardins).sample
+  event.description = Faker::Lorem.paragraph
+  event.save
+  events << event
+end
+puts "Done ! #{events.count} events added to the database."
+
+
+jobs = []
+15.times do
+  job = Job.new
+  job.price = %w(20 30 40 50 60 70 80 90 100).sample.to_i
+  job.event = Event.all.sample
+  job.professional = User.where(role: "Professional").sample
+  job.save
+  jobs << job
+end
+puts "Done ! #{jobs.count} jobs added to the database."
+
