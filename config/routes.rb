e7baac1e5db6f8bde203b0d1fa05d6a4bf8b820d
Gabriel Packer <ggkpacker@gmail.com>
2018-04-03 18:29:16 -0300

    event new/create/show added

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'users#index'
   devise_for :users
   resources :users, only: [:index, :show] do
-    resources :events, except: [:index]
   end
+  resources :events, except: [:index]
   resource :professionals, only: [:show, :edit, :update]
   resource :organizers, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
