1ab26b1e59a4f4aec473f7a6bf15ab70eeb92639
Gabriel Packer <ggkpacker@gmail.com>
2018-04-04 17:25:05 -0300

    dependent

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :organizer, foreign_key: "organizer_id", class_name: "User"
-  has_many :jobs, dependency: :destroy
+  has_many :jobs, dependent: :destroy
   has_many :pictures
   accepts_nested_attributes_for :pictures, \
   :reject_if => proc {|attributes| attributes['filename'].blank? \
