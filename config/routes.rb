10d89c21a80364801ceb5eb2c14a08d505e7b64a
Charlotte Boucher <charlotte.boucher@hec.edu>
2018-04-04 15:09:27 -0300

    link send proposal

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
   end
   resources :events, except: [:index] do
-    resources :jobs, only: [:new, :create]
   end
-  resources :jobs, only: [:show, :edit, :update, :destroy]
+  resources :jobs, only: [:new, :create, :show, :edit, :update, :destroy]
   resource :professionals, only: [:show, :edit, :update]
   resource :organizers, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
