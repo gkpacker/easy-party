c36d80305d21f4d37c75452317606a60e6a0da62
Charlotte Boucher <charlotte.boucher@hec.edu>
2018-04-04 13:32:44 -0300

    start CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,38 @@
 class JobsController < ApplicationController
+  before_action :find_job, only: [:edit, :update, :delete]
+
   def index
     @jobs = Job.select(params[:event_id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+  end
+
+  def edit
+  end
+
+  def update
+    if @job.update(job_params)
+      redirect_to event_path(job.event)
+  end
+
+  def delete
   end
 
   private
 
   def job_params
-    params.require(:job).permit(:event_id)
+    params.require(:job).permit(:event_id, :professional_id, :price, :accepted?, :done?)
+  end
+
+  private
+
+  def find_job
+    @job = Job.find(params[:event_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
   end
   resources :events, except: [:index] do
-    resources :jobs, only: [:index]
+    resources :jobs, only: [:new, :create]
   end
+  resources :jobs, only: [:show, :edit, :update, :destroy]
   resource :professionals, only: [:show, :edit, :update]
   resource :organizers, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
