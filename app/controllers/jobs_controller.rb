02c8cbaaaa5ec61c38c0bfa899cf48a17d7d477d
Gabriel Packer <ggkpacker@gmail.com>
2018-04-06 01:19:36 -0300

    some ux improved and view logic refactor

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,8 +17,11 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
     @job.professional = User.find(params[:user_id])
     authorize @job
-    @job.save
-    redirect_to event_path(@job.event)
+    if @job.save
+      redirect_to event_path(@job.event)
+    else
+      render :new
+    end
   end
 
   def edit
